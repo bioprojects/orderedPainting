f908568ec1828940e21e9b1cc0497c0d4264d435
bioprojects <k-yahara@gakushikai.jp>
2013-11-28 17:12:09 +0000

    no message

--- lib/visualization2.R
+++ lib/visualization2.R
@@ -1,5 +1,5 @@
 #
-# R --vanilla --quiet < lib/visualization.R --args lib/plotHeatmap.R  pos_matrix.txt  min  max
+# R --vanilla --quiet < lib/visualization.R --args lib/plotHeatmap.R  pos_matrix.txt  min  max [ visualStrainOrder.list ]
 #
 library(MASS)
 
@@ -12,19 +12,25 @@ source(c_args[1])
 
 ###################################################
 
-pos_matrixfile <- c_args[2]
-if (!file.exists(pos_matrixfile)) {
-  stop(sprintf("Error: %s doesn't exist", pos_matrixfile))
+pos_matrixFile <- c_args[2]
+if (!file.exists(pos_matrixFile)) {
+  stop(sprintf("Error: %s doesn't exist", pos_matrixFile))
 }
 
 min_among_sites <- as.numeric(c_args[3])
 max_among_sites <- as.numeric(c_args[4])
 
+visual_orderFile <- ""
+if (length(c_args) > 4) {
+  visual_orderFile <- c_args[5]
+}
+
+
 #
 # read
 # type distRankDesc pos ... (name of each strain)
 #
-d_each_target_site <- read.table(pos_matrixfile,header=T)
+d_each_target_site <- read.table(pos_matrixFile,header=T)
 
 #
 # set min and max (in the same scale)
@@ -42,13 +48,27 @@ if (max_among_sites < abs_min_among_sites) {
 #
 # plot heatmaps
 #
-outprefix <- sub(".txt","", pos_matrixfile)
+outprefix <- sub(".txt","", pos_matrixFile)
 
 pos          <- d_each_target_site[1,]$pos
 type         <- d_each_target_site[1,]$type
 distRankDesc <- d_each_target_site[1,]$distRankDesc
 
 d_each_for_plot <- d_each_target_site[,-c(1:3)]
+
+if (visual_orderFile != "") {
+  c_visual_order <- c()
+
+  names(d_each_for_plot) <- sub("^X","",names(d_each_for_plot))
+
+  d_visual_order <- read.table(visual_orderFile, header=F)
+  for (each in d_visual_order[,1]) {
+    c_visual_order <- c(c_visual_order, which(names(d_each_for_plot) == each))
+  }
+  
+  d_each_for_plot <- d_each_for_plot[c_visual_order,c_visual_order]
+}
+
 rownames(d_each_for_plot) <- names(d_each_for_plot)
 
 png_fpath <- sprintf("%s_%s.png", outprefix, type) # "rank" is included in the outprefix
